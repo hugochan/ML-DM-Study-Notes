5a17881a84c4d3fcf3a141fd75aa28cae58dbde1
Hugo Chan <hugochan2013@gmail.com>
2015-12-05 12:28:57 -0500

    add gspan code

--- gSpan/gspan.py
+++ gSpan/gspan.py
@@ -50,15 +50,86 @@ def extend_right_most_path(dfs_codes, dataset):
                             f = (u, right_most_child + 1, labels[0], labels[1], labels[2]) # forward edges
                             exts.append((f, i))
 
-    # what if an extension occurs in one graph for multiple times?
+    # number of distinct graph ids that support tuple s
     ext_sups = []
+    exts = list(set(exts))
     extensions = [x[0] for x in exts]
     for s in set(extensions):
         sup = extensions.count(s)
         ext_sups.append((s, sup))
-    # tuple sorted order???
-    ext_sups = sorted(ext_sups, key=lambda d:d[1], reverse=True)
+    # sorting tuples in dfs order
+    ext_sups = sorted(ext_sups, cmp=dfs_code_cmp, key=lambda e:e[0])
     return ext_sups
+
+# ???????
+def get_subgraph_isomorphisms(dfs_codes, graph):
+    nodes = []
+    for x in graph:
+        nodes.extend([(x[0], x[2]), (x[1], x[3])])
+    nodes = list(set(nodes))
+    for node_id, node_label in nodes:
+        if node_label == get_node_dfs_label(0, dfs_codes):
+            pass
+
+
+def is_canonical(dfs_codes):
+    # graph corresponding to the dfs code
+    graph = get_graph_by_dfs_codes(dfs_codes)
+    canonical_dfs_code = []
+    for i in range(len(dfs_codes)):
+        ext_sups = extend_right_most_path(canonical_dfs_code, [graph]) # extensions of canonical_dfs_code
+        ext = ext_sups[0][0] # least rightmost edge extension of canonical_dfs_code
+        if dfs_code_cmp(ext, dfs_codes[i]) == -1: # ext < dfs_codes[i]
+            return False # // canonical_dfs_code is smaller, thus dfs_codes is not canonical
+        canonical_dfs_code.append(ext)
+    return True
+
+def get_graph_by_dfs_codes(dfs_codes): #????
+    pass
+
+def dfs_code_cmp(a, b):
+    ret = dfs_edge_cmp(a[:2], b[:2]) # compare edges
+    if ret == -1:
+        return -1
+    elif ret == 0: # compare labels
+        if a[2] == b[2] and a[3] == b[3] and a[4] == b[4]:
+            return 0
+        if a[2] < b[2] or (a[2] == b[2] and a[3] < b[3]) or \
+            (a[2] == b[2] and a[3] == b[3] and a[4] < b[4]):
+            return -1
+        else:
+            return 1
+    else:
+        return 1
+
+def dfs_edge_cmp(e1, e2):
+    """
+    compare dfs edge order
+    """
+    if e1[0] < e1[1] and e2[0] < e2[1]: # they are both forward edges
+        if e1[1] < e2[1] or (e1[1] == e2[1] and e1[0] > e2[0]):
+            return -1 # e1 <e e2
+        elif e1[1] == e2[1] and e1[0] == e2[0]:
+            return 0 # e1 =e e2
+        else:
+            return 1 # e1 >e e2
+    if e1[0] > e1[1] and e2[0] > e2[1]: # they are both backward edges
+        if e1[0] < e2[0] or (e1[0] == e2[0] and e1[1] < e2[1]):
+            return -1
+        elif e1[0] == e2[0] and e1[1] == e2[1]:
+            return 0
+        else:
+            return 1
+    if e1[0] < e1[1] and e2[0] > e2[1]: # e1 is a forward edge and e2 is a backward edge
+        if e1[1] <= e2[0]:
+            return -1
+        else:
+            return 1
+    if e1[0] > e1[1] and e2[0] < e2[1]: # e1 is a backward edge and e2 is a forward edge
+        if e2[1] <= e1[0]:
+            return 1
+        else:
+            return -1
 
 def get_node_dfs_label(iid, dfs_codes):
     for code in dfs_codes:
@@ -91,13 +162,6 @@ def get_neighbors(node_id, graph):
             neighbors.append(edge[0])
     return neighbors
 
-
-def get_sub_graph_isomorphisms(dfs_codes, graph):
-    pass
-
-def is_canonical(dfs_codes):
-    pass
-
 def build_right_most_path(dfs_codes):
     """
     Build the right most path through the DFS codes
@@ -111,4 +175,29 @@ def build_right_most_path(dfs_codes):
             path.append(code)
     #print path
     return path
+
+def load_data(file_in, sep=' '):
+    data = []
+    index = -1 # graph index
+    try:
+        with open(file_in, 'r') as f:
+            for i in f:
+                tmp = i.rstrip('\r\n').split(sep)
+                if tmp[0] == 't': # graph
+                    data.append([])
+                    node_label = {}
+                    index += 1
+                elif tmp[0] == 'v': # vertex
+                    node_label[tmp[1]] = tmp[2] # id -> label
+                elif tmp[0] == 'e': # edge
+                    data[index].append((tmp[1], tmp[2], node_label[tmp[1]], node_label[tmp[2]], tmp[3]))
+    except Exception, e:
+        print e
+        exit()
+    f.close()
+    return data
+
+if __name__ == '__main__':
+    dataset = load_data('exampleG.txt')
+
 
